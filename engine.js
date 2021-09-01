242158fb254823789f9e08deac0ad7fb0cf5cd74
Priyanshu Raj <priyanshuraj583@gmail.com>
2021-09-02 02:09:14 +0530

    title name updated and css fixed.

--- engine.js
+++ engine.js
@@ -12,14 +12,15 @@ document.getElementById("result").innerText = "Calculated BMI = " + res;
  
 if(res<18.5)
   document.getElementById("health").innerText = "Underweight";
-else if(res>=18.5||res<24.9)
+else if(res<24.9)
   document.getElementById("health").innerText = "Healthy";
 
-else if(res>=25.0||res<29.9)
+else if(res<29.9)
   document.getElementById("health").innerText = "Overweight";
 
 else if(res>30.0)
   document.getElementById("health").innerText = "Obesity";
 
  }
- alert("Hello..");  
+ alert("Hello..Warm Welcome");  
+ 
